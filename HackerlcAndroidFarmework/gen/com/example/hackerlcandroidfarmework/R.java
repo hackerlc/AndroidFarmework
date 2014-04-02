c5cef839eb47ce310de276133c41b066aff47bc6
hackerlcg <hackerlcg@gmail.com>
2014-04-02 10:07:16 +0800

    更新了BaseActivity  以及跳转样式

--- HackerlcAndroidFarmework/gen/com/example/hackerlcandroidfarmework/R.java
+++ HackerlcAndroidFarmework/gen/com/example/hackerlcandroidfarmework/R.java
@@ -8,7 +8,16 @@
 package com.example.hackerlcandroidfarmework;
 
 public final class R {
+    public static final class anim {
+        public static final int backin=0x7f040000;
+        public static final int backout=0x7f040001;
+        public static final int zoomin=0x7f040002;
+        public static final int zoomout=0x7f040003;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int bg_wellcome=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,22 +26,28 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f030000;
-        public static final int activity_vertical_margin=0x7f030001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f070000;
+        public static final int action_settings=0x7f0a0001;
+        public static final int imageView1=0x7f0a0000;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f030000;
+        public static final int activity_wellcome=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070002;
+        public static final int title_activity_main=0x7f070003;
     }
     public static final class style {
         /** 
@@ -56,10 +71,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f080001;
+        public static final int notitle=0x7f080002;
     }
 }
--- HackerlcAndroidFarmework/src/com/example/hackerlcandroidfarmework/HackerlcApplication.java
+++ HackerlcAndroidFarmework/src/com/example/hackerlcandroidfarmework/HackerlcApplication.java
@@ -12,7 +12,12 @@ public class HackerlcApplication extends Application {
 	/**
 	 * Log.i tag 
 	 */
-	public static String LOGTAG="++Hackerlc_Farmaework++";
+	public static final String LOGTAG="++Hackerlc_Farmaework++";
+	
+	/**
+	 * 跳转样式为inOut切入方式
+	 */
+	public static final int JUMP_STYLE=10; 
 	
 	private HackerlcApplication instance;
 
